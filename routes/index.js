0681f60b07ed7c5b0b52f8800191e1349b356c27
Garucx <90602497+Garucx@users.noreply.github.com>
2021-12-08 16:01:39 +0100

    Small Changes

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,43 @@
 var express = require("express");
 var router = express.Router();
-const { GetLocation, Durchschnitt,getStation,getStationInformation } = require("../models/app");
+var myFunctions = require("../models/app");
 
 router.get("/getData/:bezirk", async (req, res) => {
   var response;
   if(req.params.bezirk > 23 || req.params.bezirk < 1)
   {
-    response = {"ERR":"400 Wrong input"};
+    response = {"ERR":"400 Bad Request"};
     res.send(response);
     return;
   }
 
   if(req.params.bezirk.toLowerCase() =="all")
   {
-    response = await GetLocation("all");
+    response = await myFunctions.GetLocation("all");
     res.send(response);
     return;
   }
   
-  response = await GetLocation(req.params.bezirk);
+  response = await myFunctions.GetLocation(req.params.bezirk);
   res.send(response);
 });
 
 router.get("/getData/:bezirk/:info", async (req, res) => {
  
- 
   var response;
   if(req.params.info.toLowerCase() == "temp" || req.params.info.toLowerCase() == "pressure" || req.params.info.toLowerCase() == "windspeed" || req.params.info.toLowerCase() == "elevation")
     {
-        response = await Durchschnitt(req.params.bezirk,req.params.info.toLowerCase());
+        response = await myFunctions.Durchschnitt(req.params.bezirk,req.params.info.toLowerCase());
     }else
     {
-      response = {"ERR":"400 Wrong input"};
+      response = {"ERR":"400 Bad Request"};
     }
-    (response == undefined) ? response = {"ERR":"400 Wrong input"}: null;
+    (response == undefined) ? response = {"ERR":"400 Bad Request"}: null;
     res.send(response);
 });
 router.get("/getStation/:Stationid", async (req, res) => {
-  var response = await getStation(req.params.Stationid);
+  var response = await myFunctions.getStation(req.params.Stationid);
+  (response.length == 0) ? response = {"ERR":"400 Bad Request"} : null;
   res.send(response);
 });
 
@@ -45,16 +45,17 @@ router.get("/getStation/:Stationid/:info", async (req, res) => {
   var response;
     if(req.params.info.toLowerCase() == "temp" || req.params.info.toLowerCase() == "pressure" || req.params.info.toLowerCase() == "windspeed" || req.params.info.toLowerCase() == "elevation")
     {
-        response = await getStationInformation(req.params.Stationid,req.params.info.toLowerCase());
+        response = await myFunctions.getStationInformation(req.params.Stationid,req.params.info.toLowerCase());
     }else
     {
-      response = {"ERR":"400 Wrong input"};
+      response = {"ERR":"400 Bad Request"};
     }
-    (response == undefined) ? response = {"ERR":"400 Wrong input"}: null;
+    (response == undefined) ? response = {"ERR":"400 Bad Request"}: null;
   res.send(response);
 });
 
 router.get("/serverstatus", async (req, res) => {
   res.send("200");
+  
 });
 module.exports = router;
